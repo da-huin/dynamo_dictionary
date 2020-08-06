21245f3e6f1d7b18b6199697744b15cf8055aab8
Park <dahuin000@gmail.com>
2020-08-07 03:16:13 +0900

    version changed

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 # Use this command for deploy.
-#   python3 setup.py sdist bdist_wheel
-#   python3 -m twine upload --skip-existing dist/*
+#   python3 setup.py sdist bdist_wheel && python3 -m twine upload --skip-existing dist/*
 
 from setuptools import find_packages, setup
 
 setup(name='dynamo_dictionary',
-      version='1.0',
+      version='1.0.1',
       description='Easily Use DynamoDB as a key-value format.',
       long_description="Please refer to the https://github.com/da-huin/dynamo_dictionary",
       long_description_content_type="text/markdown",
